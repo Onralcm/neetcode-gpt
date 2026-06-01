4e2834cec61a69f2a77406a53bbeee75f0075000
Onur Alaçam <59671502+Onralcm@users.noreply.github.com>
2026-06-01 23:15:39 +0200

    Update Digit classifier (Training 4/4)

--- foundations/digit_classifier.py
+++ foundations/digit_classifier.py
@@ -6,19 +6,19 @@ class Solution(nn.Module):
     def __init__(self):
         super().__init__()
         torch.manual_seed(0)
-        self.first_layer = nn.Linear(784, 512)
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(0.2)
-        self.final_layer = nn.Linear(512, 10)
-        self.sigmoid = nn.Sigmoid()
-        # Define the architecture here
-    
+        # Architecture: Linear(784, 512) -> ReLU -> Dropout(0.2) -> Linear(512, 10) -> Sigmoid
+        self.model = nn.Sequential(
+            nn.Linear(784, 512), nn.ReLU(), nn.Dropout(0.2),
+            nn.Linear(512, 10), nn.Sigmoid()
+        )  
+
     def forward(self, images: TensorType[float]) -> TensorType[float]:
         torch.manual_seed(0)
-        x = self.first_layer(images)
-        x = self.relu(x)
-        x = self.dropout(x)
-        x = self.final_layer(x)
-        x = self.sigmoid(x)
-        return torch.round(x, decimals = 4)
+        # images shape: (batch_size, 784)
         # Return the model's prediction to 4 decimal places
+        predictions = []
+        for i in range(images.size(0)):
+            out = torch.round(self.model(images[i, :].flatten()), decimals=4)
+            predictions.append(out)
+
+        return torch.stack(predictions)
